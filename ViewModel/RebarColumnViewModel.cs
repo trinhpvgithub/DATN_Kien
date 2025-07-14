686a6b1ec56e2ec763eaf2663eea93f8731d27c0
Pham Trinh <trinhtrong0505@gmail.com>
2025-07-14 12:53:03 +0700

    update

--- ViewModel/RebarColumnViewModel.cs
+++ ViewModel/RebarColumnViewModel.cs
@@ -1,22 +1,35 @@
 ﻿using DATN_Kien.View;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DATN_Kien.ViewModel
 {
-    public partial class RebarColumnViewModel: ObservableObject
+	public partial class RebarColumnViewModel : ObservableObject
 	{
 		public MainView View { get; set; }
 		[ObservableProperty]
-        private bool _isRound = false;
+		private bool _isRound;
 		[ObservableProperty]
-		private bool _isMirror = true;
+		private bool _isMirror;
+		[ObservableProperty]
+		private bool _level1;
+		[ObservableProperty]
+		private bool _level2;
+		[ObservableProperty]
+		private bool _level3;
+		[ObservableProperty]
+		private string _imagePath = "pack://application:,,,/Resources/Images/RebarColumn.png";
 		public RebarColumnViewModel()
 		{
 			// Constructor logic if needed
+			Level1 = true;
+			Level2 = true;
+			Level3 = true;
+			IsRound = true;
 		}
 		public void Run()
 		{
@@ -24,5 +37,32 @@ namespace DATN_Kien.ViewModel
 			View.DataContext = this;
 			View.ShowDialog();
 		}
+		protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+		{
+			base.OnPropertyChanged(e);
+			// Additional logic if needed when a property changes
+			if (e.PropertyName == nameof(IsRound) || e.PropertyName == nameof(IsMirror))
+			{
+				if (IsRound)
+				{
+					Level1 = true;
+					Level2 = false;
+					Level3 = false;
+				}
+				else
+				{
+					Level1 = true;
+					Level2 = true;
+					Level3 = true;
+				}
+				SetImagePath();
+			}
+		}
+		private void SetImagePath()
+		{
+			if (IsRound) ImagePath = "Resources/chuvi.png";
+			else ImagePath = "Resources/doixung.png";
+		}
+
 	}
 }
